f6de813ca99ed21d60274d5a5031bfbf7affd809
MathisB <mathis.bicheyre@qapa.com>
2023-12-01 23:56:23 +0100

    using logger

--- app/discord/connector.ts
+++ app/discord/connector.ts
@@ -2,6 +2,8 @@ import fs from "fs";
 import path from "path";
 import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
 
+import { warning, info } from "../logger/logger";
+
 export default function connectToDiscord() {
   const token = process.env.DISCORD_TOKEN;
   const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -17,17 +19,19 @@ export default function connectToDiscord() {
       const filePath = path.join(commandsPath, file);
       const command = require(filePath);
       if ("data" in command && "execute" in command) {
+        info("Successfully parsed command %s", command.data.name);
         commands.set(command.data.name, command);
       } else {
-        console.log(
-          `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+        warning(
+          "The command at %s is missing a required data or execute property",
+          filePath
         );
       }
     }
   }
 
   client.once(Events.ClientReady, (readyClient) => {
-    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+    info(`Ready! Logged in as ${readyClient.user.tag}`);
   });
 
   client.login(token);
